a82f4a62c6609f72306e3e63348debe7aeb6c8ed
James Fillingham <james.filling@thepixel.com>
2023-09-09 19:07:07 +0300

    parsing arguments

---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+use clap::{Arg, ArgAction, Command};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool,
+    number_non_blank_line: bool,
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    dbg!(config);
+    Ok(())
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let args = Command::new("catr")
+        .version("0.1")
+        .author("jjjame")
+        .about("rust cat")
+        .arg(
+            Arg::new("files")
+                .value_name("FILES")
+                .help("files to cat")
+                .action(ArgAction::Append)
+                .required(true),
+        )
+        .arg(
+            Arg::new("number")
+                .value_name("NUMBER")
+                .help("display line numbers")
+                .short('n')
+                .num_args(0),
+        )
+        .arg(
+            Arg::new("number_non_blank")
+                .value_name("NUMBER_NON_BLANK")
+                .help("display line numbers non blank")
+                .short('b')
+                .num_args(0),
+        )
+        .get_matches();
+
+    let files: Vec<String> = args
+        .get_many::<String>("files")
+        .unwrap()
+        .filter_map(|s: &String| s.parse().ok())
+        .collect();
+
+    Ok(Config {
+        files,
+        number_lines: args.get_flag("number"),
+        number_non_blank_line: args.get_flag("number_non_blank"),
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use catr;
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = catr::get_args().and_then(catr::run) {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
 }
